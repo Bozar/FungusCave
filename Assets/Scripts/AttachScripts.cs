4bdc2a65a7098f46e1c21466bffef6f2f4464099
Bozar <5583771+Bozar@users.noreply.github.com>
2018-10-07 19:22:11 +0800

    Add components to GameLogic by script.

---
+++ Assets/Scripts/AttachScripts.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class AttachScripts : MonoBehaviour
+{
+    private void Awake()
+    {
+        gameObject.AddComponent<Singleton>();
+        gameObject.AddComponent<BuildDungeon>();
+        gameObject.AddComponent<UserInput>();
+        gameObject.AddComponent<Move>();
+        gameObject.AddComponent<FindObjects>();
+    }
+}
