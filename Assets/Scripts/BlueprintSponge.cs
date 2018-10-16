806a5fad49d551de97ccc4190426e21b78d956d1
Bozar <5583771+Bozar@users.noreply.github.com>
2018-10-16 17:54:20 +0800

    Dig through wall block.

--- Assets/Scripts/BlueprintSponge.cs
+++ Assets/Scripts/BlueprintSponge.cs
@@ -1,15 +1,18 @@
-﻿public class BlueprintSponge : DungeonBlueprint, DungeonBlueprint.IIsEmptyArea
+﻿using System.Collections.Generic;
 
+public class BlueprintSponge : DungeonBlueprint, DungeonBlueprint.IIsEmptyArea
 {
+    private int digX;
+    private int digY;
     private int height;
     private int[] index;
     private int maxHeight;
     private int maxWidth;
     private int minHeight;
     private int minWidth;
+    private int startX;
+    private int startY;
     private int width;
-    private int x;
-    private int y;
 
     public bool IsEmptyArea(int x, int y, int width, int height)
     {
@@ -42,39 +45,111 @@
     public void Test()
     {
         SolidWall();
+        DigTunnel();
     }
 
     private void Awake()
     {
         minWidth = 4;
         minHeight = 4;
-        maxWidth = 8;
-        maxHeight = 8;
+        maxWidth = 7;
+        maxHeight = 7;
         index = new int[2];
+    }
+
+    private void DigTunnel()
+    {
+        // From bottom to top.
+        if (width >= height)
+        {
+            digX = random.RNG.Next(startX, startX + width);
+            digY = startY;
+
+            while (digX > -1 && digY > -1)
+            {
+                board.ChangeBlock(DungeonBoard.DungeonBlock.Floor, digX, digY);
+
+                digX = NextStep(digX, startX, startX + width - 1);
+                digY = NextStep(digY, startY, startY + height - 1, 1);
+            }
+        }
+        // From left to right.
+        else
+        {
+            digX = startX;
+            digY = random.RNG.Next(startY, startY + height);
+
+            while (digX > -1 && digY > -1)
+            {
+                board.ChangeBlock(DungeonBoard.DungeonBlock.Floor, digX, digY);
+
+                digX = NextStep(digX, startX, startX + width - 1, 1);
+                digY = NextStep(digY, startY, startY + height - 1);
+            }
+        }
+    }
+
+    private int NextStep(int current, int min, int max, int step)
+    {
+        int next = current + step;
+
+        if (next >= min && next <= max)
+        {
+            return next;
+        }
+        return -1;
+    }
+
+    private int NextStep(int current, int min, int max)
+    {
+        int next;
+        int stepIndex;
+        List<int> step = new List<int> { -1, 0, 1 };
+
+        while (step.Count > 0)
+        {
+            stepIndex = random.RNG.Next(0, step.Count);
+            next = current + step[stepIndex];
+
+            if (next >= min && next <= max)
+            {
+                return next;
+            }
+
+            step.RemoveAt(stepIndex);
+        }
+
+        return -1;
     }
 
     private void SolidWall()
     {
+        bool maxSize;
+
         do
         {
             index = RandomIndex();
-            x = index[0];
-            y = index[1];
+            startX = index[0];
+            startY = index[1];
 
             width = random.RNG.Next(minWidth, maxWidth + 1);
             height = random.RNG.Next(minHeight, maxHeight + 1);
-        } while (!IsEmptyArea(x, y, width, height));
+
+            maxSize = width == maxWidth && height == maxHeight;
+        } while (
+        !IsEmptyArea(startX, startY, width, height)
+        && maxSize);
 
         // Shrink the solid wall block by 1 grid so that you can always walk
         // around it.
-        x += 1;
-        y += 1;
+        startX += 1;
+        startY += 1;
         width -= 2;
         height -= 2;
 
-        for (int i = x; i < x + width; i++)
+        for (int i = startX; i < startX + width; i++)
         {
-            for (int j = y; j < y + height; j++)
+            for (int j = startY; j < startY + height; j++)
             {
                 board.ChangeBlock(DungeonBoard.DungeonBlock.Wall, i, j);
             }
