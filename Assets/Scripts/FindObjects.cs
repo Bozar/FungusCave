d5c5e3f7a796b93f7ed37d706fa1b60384c6e540
Bozar <5583771+Bozar@users.noreply.github.com>
2018-10-17 17:21:50 +0800

    Set FindObjects.GameLogic in Awake.

--- Assets/Scripts/FindObjects.cs
+++ Assets/Scripts/FindObjects.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 
-// A helper class that stores references to other game objects.
+// A helper class that stores references to other game objects. The ONLY game
+// object which it can be and MUST be attached to is GameLogic.
 public class FindObjects : MonoBehaviour
 {
     public static GameObject GameLogic { get; private set; }
     public static GameObject[] MainUIList { get; private set; }
 
-    private void Start()
+    private void Awake()
     {
         // Object.
-        GameLogic = GameObject.FindGameObjectWithTag("GameLogic");
+        GameLogic = gameObject;
+    }
 
+    private void Start()
+    {
         // ObjectS.
         MainUIList = GameObject.FindGameObjectsWithTag("MainUI");
     }
