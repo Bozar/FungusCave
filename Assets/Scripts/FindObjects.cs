ecd9deb703fd73cd979ef85bc09a71fdcac20e3c
Bozar <5583771+Bozar@users.noreply.github.com>
2018-10-20 14:37:09 +0800

    Store UI object references in a dictionary.

--- Assets/Scripts/FindObjects.cs
+++ Assets/Scripts/FindObjects.cs
@@ -1,21 +1,43 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 // A helper class that stores references to other game objects. The ONLY game
 // object which it can be and MUST be attached to is GameLogic.
 public class FindObjects : MonoBehaviour
 {
+    private int tempDictKey;
+    private GameObject[] tempGOArray;
+
+    public enum UITags { NONE, Seed };
+
     public static GameObject GameLogic { get; private set; }
-    public static GameObject[] MainUIList { get; private set; }
+    public static Dictionary<int, GameObject> MainUIDict { get; private set; }
 
     private void Awake()
     {
         // Object.
         GameLogic = gameObject;
+
+        MainUIDict = new Dictionary<int, GameObject>();
     }
 
     private void Start()
     {
         // ObjectS.
-        MainUIList = GameObject.FindGameObjectsWithTag("MainUI");
+        tempGOArray = GameObject.FindGameObjectsWithTag("MainUI");
+
+        foreach (var go in tempGOArray)
+        {
+            if (Enum.IsDefined(typeof(UITags), go.name))
+            {
+                tempDictKey = (int)Enum.Parse(typeof(UITags), go.name);
+
+                if (!MainUIDict.ContainsKey(tempDictKey))
+                {
+                    MainUIDict.Add(tempDictKey, go);
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UserInterface : MonoBehaviour
 {
     private void Update()
     {
+        UpdateSeed();
+    }
+
+    private void UpdateSeed()
+    {
+        FindObjects.MainUIDict[(int)FindObjects.UITags.Seed].
+            GetComponent<Text>().text = "54321";
     }
 }
