001b8c87668ce6bfa995323c3db39abc2a6ae7b1
Bozar <5583771+Bozar@users.noreply.github.com>
2018-11-05 13:00:37 +0800

    Create an actor using ObjectPool.

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ObjectTag { TEMP, Dummy, PC };
+public enum ObjectTag
+{
+    TEMP, TActor, TBuilding,
+    PC, Dummy,
+    Wall, Pool, Fungus
+};
 
 public class ObjectPool : MonoBehaviour
 {
     private Dictionary<ObjectTag, Stack<GameObject>> pool;
+
+    public GameObject CreateObject(ObjectTag tag, int x, int y)
+    {
+        switch (ClassifyTag(tag))
+        {
+            case ObjectTag.TActor:
+                return CreateNPC(tag, x, y);
+
+            case ObjectTag.TBuilding:
+                return null;
+
+            case ObjectTag.PC:
+                return CreatePC(x, y);
+
+            default:
+                return null;
+        }
+    }
 
     private void Awake()
     {
@@ -16,5 +39,77 @@ public class ObjectPool : MonoBehaviour
         {
             pool.Add((ObjectTag)tag, new Stack<GameObject>());
         }
+    }
+
+    private ObjectTag ClassifyTag(ObjectTag tag)
+    {
+        switch (tag)
+        {
+            case ObjectTag.Wall:
+            case ObjectTag.Pool:
+            case ObjectTag.Fungus:
+                return ObjectTag.TBuilding;
+
+            case ObjectTag.Dummy:
+                return ObjectTag.TActor;
+
+            default:
+                return tag;
+        }
+    }
+
+    private GameObject CreateNPC(ObjectTag tag, int x, int y)
+    {
+        GameObject go;
+
+        if (pool[tag].Count > 0)
+        {
+            // TODO: Refresh the actor.
+            go = pool[tag].Pop();
+
+            return go;
+        }
+
+        go = Instantiate(Resources.Load(tag.ToString()) as GameObject);
+
+        // TODO: Remove PC specific components.
+        go.AddComponent<PlayerInput>();
+        go.AddComponent<PCActions>().enabled = false;
+
+        go.AddComponent<ObjectMetaInfo>();
+        go.GetComponent<ObjectMetaInfo>().TagName = tag;
+
+        // ObjectPool is attached to GameLogic.
+        go.transform.position
+            = gameObject.GetComponent<ConvertCoordinates>().Convert(x, y);
+
+        go.AddComponent<FieldOfView>();
+        go.AddComponent<FOVRhombus>();
+        //go.AddComponent<FOVSimple>();
+        go.AddComponent<RenderSprite>();
+
+        go.AddComponent<TileOverlay>();
+        go.AddComponent<InternalClock>();
+
+        go.AddComponent<Energy>();
+        go.AddComponent<Move>();
+        go.AddComponent<Attack>();
+        go.AddComponent<Defend>();
+
+        gameObject.GetComponent<ActorBoard>().AddActor(go, x, y);
+        gameObject.GetComponent<SchedulingSystem>().AddActor(go);
+
+        return go;
+    }
+
+    private GameObject CreatePC(int x, int y)
+    {
+        GameObject go;
+
+        // TODO: Add PC specific components.
+        go = CreateNPC(ObjectTag.PC, x, y);
+        go.GetComponent<PCActions>().enabled = true;
+
+        return go;
     }
 }
--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,13 +5,10 @@ public class Test : MonoBehaviour
 {
     public static readonly int height = 17;
     public static readonly int width = 24;
-    private GameObject dummyTile;
+
     private GameObject[] mainUI;
     private Text message;
 
-    private GameObject newDummy;
-    private GameObject newPC;
-    private GameObject pcTile;
     public bool RenderAll { get; set; }
 
     private void Awake()
@@ -21,56 +18,13 @@ public class Test : MonoBehaviour
 
     private void Start()
     {
-        pcTile = Resources.Load(ObjectTag.PC.ToString()) as GameObject;
-        dummyTile = Resources.Load(ObjectTag.Dummy.ToString()) as GameObject;
-        //dummyTile = Resources.Load("Dummy") as GameObject;
-
-        newPC = Instantiate(pcTile);
-        newPC.transform.position = new Vector3(0, 0);
-        newPC.AddComponent<ObjectMetaInfo>();
-        newPC.GetComponent<ObjectMetaInfo>().TagName = ObjectTag.PC;
-
-        newPC.AddComponent<PCActions>();
-        newPC.AddComponent<TileOverlay>();
-        newPC.AddComponent<FieldOfView>();
-        newPC.AddComponent<RenderSprite>();
-        newPC.AddComponent<FOVRhombus>();
-        newPC.AddComponent<FOVSimple>();
-        newPC.AddComponent<Energy>();
-        newPC.AddComponent<PlayerInput>();
-        newPC.AddComponent<Move>();
-        newPC.AddComponent<InternalClock>();
-        newPC.AddComponent<Attack>();
-        newPC.AddComponent<Defend>();
-
-        gameObject.GetComponent<SchedulingSystem>().AddActor(newPC);
-        gameObject.GetComponent<ActorBoard>().AddActor(newPC, 0, 0);
+        FindObjects.GameLogic.GetComponent<ObjectPool>()
+            .CreateObject(ObjectTag.PC, 0, 0);
 
         for (int i = 0; i < 2; i++)
         {
-            newDummy = Instantiate(dummyTile);
-            newDummy.transform.position
-                   = gameObject.GetComponent<ConvertCoordinates>()
-                   .Convert(i * 2, i + 1);
-
-            gameObject.GetComponent<ActorBoard>().AddActor(
-                newDummy, i * 2, i + 1);
-
-            newDummy.AddComponent<TileOverlay>();
-            //newDummy.AddComponent<FieldOfView>();
-            newDummy.AddComponent<RenderSprite>();
-            newDummy.AddComponent<PlayerInput>();
-            newDummy.AddComponent<PCActions>().enabled = false;
-            newDummy.AddComponent<Move>();
-            newDummy.AddComponent<Energy>();
-            newDummy.AddComponent<InternalClock>();
-            newDummy.AddComponent<Attack>();
-            newDummy.AddComponent<Defend>();
-
-            newDummy.AddComponent<ObjectMetaInfo>();
-            newDummy.GetComponent<ObjectMetaInfo>().TagName = ObjectTag.Dummy;
-
-            gameObject.GetComponent<SchedulingSystem>().AddActor(newDummy);
+            FindObjects.GameLogic.GetComponent<ObjectPool>()
+                .CreateObject(ObjectTag.Dummy, i * 2, i + 1);
         }
 
         mainUI = GameObject.FindGameObjectsWithTag("MainUI");
