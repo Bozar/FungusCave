a25f0d07790911bd201b7c351ac62cc82a52ae32
Bozar <5583771+Bozar@users.noreply.github.com>
2018-11-15 16:15:50 +0800

    "Overload" Random.Next.

--- Assets/Scripts/RandomNumber.cs
+++ Assets/Scripts/RandomNumber.cs
@@ -13,6 +13,8 @@ public enum SeedTag
 
 public class RandomNumber : MonoBehaviour
 {
+    private Dictionary<SeedTag, Queue<int>> intQueueDict;
+    private Dictionary<SeedTag, System.Random> rngDict;
     private Dictionary<SeedTag, int> seedDict;
     private List<SeedTag> tagList;
 
@@ -25,7 +27,7 @@ public class RandomNumber : MonoBehaviour
 
         if (RootSeed == 0)
         {
-            RootSeed = RandomSeed();
+            RootSeed = RandomInteger(true);
         }
 
         RNG = new System.Random(RootSeed);
@@ -35,7 +37,7 @@ public class RandomNumber : MonoBehaviour
         {
             if (tag != SeedTag.Root)
             {
-                seedDict[tag] = RandomSeed(RNG);
+                seedDict[tag] = RandomInteger(false, RNG);
             }
             else
             {
@@ -47,38 +49,136 @@ public class RandomNumber : MonoBehaviour
         {
             Debug.Log(tagSeed);
         }
+
+        foreach (var tagRNG in rngDict)
+        {
+            Debug.Log(tagRNG);
+        }
+
+        foreach (var tagRNG in intQueueDict)
+        {
+            Debug.Log(tagRNG);
+        }
+
+        Debug.Log(Next(SeedTag.Dungeon));
+        Debug.Log(Next(SeedTag.AutoExplore));
+        Debug.Log(Next(SeedTag.Dungeon, 1, 11));
+        //Debug.Log(Next(SeedTag.PERSISTENT));
+    }
+
+    public double Next(SeedTag tag)
+    {
+        if (IsInvalid(tag))
+        {
+            throw new Exception("Invalid tag: " + tag);
+        }
+
+        InitializeRNG(tag);
+
+        if (IsPersistent(tag))
+        {
+            return -42;
+        }
+
+        return rngDict[tag].NextDouble();
+    }
+
+    public int Next(SeedTag tag, int min, int max)
+    {
+        if (IsInvalid(tag))
+        {
+            throw new Exception("Invalid tag: " + tag);
+        }
+
+        InitializeRNG(tag);
+
+        if (IsPersistent(tag))
+        {
+            return -42;
+        }
+
+        return rngDict[tag].Next(min, max);
+    }
+
+    public int Next(SeedTag tag, int max)
+    {
+        return Next(tag, 0, max);
     }
 
     private void Awake()
     {
         seedDict = new Dictionary<SeedTag, int>();
+        rngDict = new Dictionary<SeedTag, System.Random>();
+        intQueueDict = new Dictionary<SeedTag, Queue<int>>();
 
         foreach (var tag in Enum.GetValues(typeof(SeedTag)))
         {
-            if (!tag.Equals(SeedTag.DISCARDABLE)
-                && !tag.Equals(SeedTag.PERSISTENT))
+            if (IsInvalid((SeedTag)tag))
             {
-                seedDict.Add((SeedTag)tag, -1);
+                continue;
+            }
+
+            seedDict.Add((SeedTag)tag, -1);
+
+            if (IsPersistent((SeedTag)tag))
+            {
+                intQueueDict.Add((SeedTag)tag, new Queue<int>());
+            }
+            else
+            {
+                rngDict.Add((SeedTag)tag, null);
             }
         }
     }
 
-    private int RandomSeed()
+    private void InitializeRNG(SeedTag tag)
+    {
+        if (IsInvalid(tag))
+        {
+            return;
+        }
+
+        if (IsPersistent(tag))
+        {
+            Debug.Log("WIP");
+        }
+        else
+        {
+            if (rngDict[tag] == null)
+            {
+                rngDict[tag] = new System.Random(seedDict[tag]);
+            }
+        }
+    }
+
+    private bool IsInvalid(SeedTag tag)
+    {
+        bool isDiscardable = tag.Equals(SeedTag.DISCARDABLE);
+        bool isPersistent = tag.Equals(SeedTag.PERSISTENT);
+
+        return isDiscardable || isPersistent;
+    }
+
+    private bool IsPersistent(SeedTag tag)
+    {
+        return tag.CompareTo(SeedTag.PERSISTENT) > 0;
+    }
+
+    private int RandomInteger(bool mustHave9Digits)
+    {
+        return RandomInteger(mustHave9Digits, new System.Random());
+    }
+
+    private int RandomInteger(bool mustHave9Digits, System.Random rng)
     {
         double doubleSeed;
-        System.Random rng = new System.Random();
 
         do
         {
             doubleSeed = rng.NextDouble();
         }
-        while (doubleSeed < 0.1);
+        while (mustHave9Digits && (doubleSeed < 0.1));
 
         return (int)(doubleSeed * Math.Pow(10, 9));
-    }
-
-    private int RandomSeed(System.Random rng)
-    {
-        return (int)(rng.NextDouble() * Math.Pow(10, 9));
     }
 }
