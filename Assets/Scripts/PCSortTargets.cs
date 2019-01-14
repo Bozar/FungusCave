fe5fc7108fc587446340b7a1e570444da11091c4
Bozar <5583771+Bozar@users.noreply.github.com>
2019-01-14 20:56:39 +0800

    Find & split targets in sight.

--- Assets/Scripts/PCSortTargets.cs
+++ Assets/Scripts/PCSortTargets.cs
@@ -1,20 +1,99 @@
-﻿using System.Collections.Generic;
+﻿using Fungus.Actor.FOV;
+using Fungus.GameSystem;
+using Fungus.GameSystem.WorldBuilding;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Fungus.Actor.AI
 {
-    public interface ISortTargets { List<GameObject> GetTargetsInSight(); }
+    public interface ISortTargets
+    {
+        List<GameObject> GetTargetsInSight();
+    }
 
     public class PCSortTargets : MonoBehaviour, ISortTargets
     {
+        private ActorBoard actor;
+        private ConvertCoordinates coord;
+        private FieldOfView fov;
+
         public List<GameObject> GetTargetsInSight()
         {
-            //> Store actors in sight.
-            //> Classify actors into 4 lists.
             //> Sort actors.
             //> Combine lists and return the result.
 
-            return new List<GameObject>();
+            List<GameObject> targets = FindTargets();
+
+            if (targets.Count < 2)
+            {
+                return targets;
+            }
+
+            List<GameObject>[] splitted = SplitTargets(targets);
+            List<GameObject> left = splitted[0];
+            List<GameObject> right = splitted[1];
+
+            return targets;
+        }
+
+        private List<GameObject> FindTargets()
+        {
+            int[] position = coord.Convert(transform.position);
+            int x = position[0];
+            int y = position[1];
+            int range = fov.MaxRange;
+
+            List<GameObject> targets = new List<GameObject>();
+
+            for (int i = x - range; i < x + range + 1; i++)
+            {
+                for (int j = y - range; j < y + range + 1; j++)
+                {
+                    if (!actor.HasActor(i, j)
+                        || !fov.CheckFOV(FOVStatus.Insight, i, j)
+                        || (i == x && j == y))
+                    {
+                        continue;
+                    }
+
+                    targets.Add(actor.GetActor(i, j));
+                }
+            }
+
+            return targets;
+        }
+
+        private List<GameObject>[] SplitTargets(List<GameObject> targets)
+        {
+            int sourceX = coord.Convert(gameObject.transform.position)[0];
+
+            int targetX;
+            List<GameObject> left = new List<GameObject>();
+            List<GameObject> right = new List<GameObject>();
+
+            foreach (GameObject target in targets)
+            {
+                targetX = coord.Convert(target.transform.position)[0];
+
+                if (targetX <= sourceX)
+                {
+                    left.Add(target);
+                }
+                else
+                {
+                    right.Add(target);
+                }
+            }
+
+            return new List<GameObject>[] { left, right };
+        }
+
+        private void Start()
+        {
+            coord = FindObjects.GameLogic.GetComponent<ConvertCoordinates>();
+            actor = FindObjects.GameLogic.GetComponent<ActorBoard>();
+
+            fov = GetComponent<FieldOfView>();
         }
     }
 }
