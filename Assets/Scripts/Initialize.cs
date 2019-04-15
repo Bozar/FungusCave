611632060323e9437ba8a81e744c57e8dd849591
Bozar <5583771+Bozar@users.noreply.github.com>
2019-04-15 22:11:07 +0800

    Test: binary serialization.

--- Assets/Scripts/Initialize.cs
+++ Assets/Scripts/Initialize.cs
@@ -1,4 +1,9 @@
 ﻿using Fungus.GameSystem.WorldBuilding;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -35,6 +40,41 @@ namespace Fungus.GameSystem
 
             GetComponent<SubMode>().SwitchModeOpening(
                 GetComponent<GameSetting>().ShowOpening);
+
+            //TestSave();
+            //TestLoad();
+        }
+
+        private void TestLoad()
+        {
+            IFormatter formatter = new BinaryFormatter();
+            MySave ms;
+
+            using (FileStream s = File.OpenRead("serialized.bin"))
+            {
+                ms = (MySave)formatter.Deserialize(s);
+            }
+
+            Debug.Log(ms.MyNum);
+            Debug.Log(ms.MyStr);
+
+            Debug.Log(ms.MyDict[0].MyNum);
+            Debug.Log(ms.MyDict[0].MyStr);
+        }
+
+        private void TestSave()
+        {
+            MySave ms = new MySave { };
+            ms.MyNum = 42;
+            ms.MyStr = "Hello world";
+            ms.MyDict = new Dictionary<int, MySave>
+            { { 0, new MySave { MyNum = 24, MyStr = "Good night" } } };
+            IFormatter formatter = new BinaryFormatter();
+
+            using (FileStream s = File.Create("serialized.bin"))
+            { formatter.Serialize(s, ms); }
+
+            Debug.Log("File save");
         }
 
         private void Update()
@@ -44,5 +84,13 @@ namespace Fungus.GameSystem
                 InitializeGame();
             }
         }
+    }
+
+    [Serializable]
+    public class MySave
+    {
+        public int MyNum;
+        public string MyStr;
+        internal Dictionary<int, MySave> MyDict;
     }
 }
