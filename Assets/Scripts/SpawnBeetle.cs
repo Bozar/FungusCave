ffe9a06e98421d67ea8bbef4f43e0adf970ebf66
bozar <5583771+Bozar@users.noreply.github.com>
2019-04-30 14:11:53 +0800

    WIP: Spawn beetles around PC.

--- Assets/Scripts/SpawnBeetle.cs
+++ Assets/Scripts/SpawnBeetle.cs
@@ -2,6 +2,7 @@
 using Fungus.GameSystem.Render;
 using Fungus.GameSystem.WorldBuilding;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Fungus.GameSystem.Progress
@@ -11,6 +12,7 @@ namespace Fungus.GameSystem.Progress
         private readonly string dataNode = "SpawnBeetle";
         private readonly string textNode = "SpawnBeetle";
         private int count;
+        private int distance;
         private int max;
         private bool notWarned;
 
@@ -25,12 +27,80 @@ namespace Fungus.GameSystem.Progress
             else if (count < 1)
             {
                 StoreMessage("Emerge");
-
-                // TODO: Spawn beetles.
+                CreateBeetle();
 
                 count = max;
                 notWarned = true;
             }
+        }
+
+        private void CreateBeetle()
+        {
+            int[][] position = GetPosition();
+            position = FilterPosition(position);
+            if (position == null)
+            {
+                return;
+            }
+
+            DungeonLevel dl = GetComponent<DungeonProgressData>()
+                .GetDungeonLevel();
+            SubObjectTag minion = GetComponent<ActorGroupData>()
+                .GetActorGroup(dl)[CombatRoleTag.Minion][0];
+
+            foreach (int[] p in position)
+            {
+                GetComponent<ObjectPool>().CreateObject(
+                    MainObjectTag.Actor,
+                    minion,
+                    p);
+            }
+        }
+
+        private int[][] FilterPosition(int[][] position)
+        {
+            int maxBeetle = GetComponent<GameData>().GetIntData(dataNode,
+                "Beetle");
+            // TODO: Count remaining enemies.
+            if (position.Length < maxBeetle)
+            {
+                return null;
+            }
+
+            SeedTag seed = GetComponent<DungeonProgressData>().GetDungeonSeed();
+            int[][] randomized = position.OrderBy(
+                p => GetComponent<RandomNumber>().Next(seed))
+                .ToArray();
+            Stack<int[]> result = new Stack<int[]>();
+
+            for (int i = 0; i < maxBeetle; i++)
+            {
+                result.Push(randomized[i]);
+            }
+            return result.ToArray();
+        }
+
+        private int[][] GetPosition()
+        {
+            int[] pcPosition = GetComponent<ConvertCoordinates>().Convert(
+                FindObjects.PC.transform.position);
+            Stack<int[]> position = new Stack<int[]>();
+
+            for (int i = pcPosition[0] - distance;
+                i < pcPosition[0] + distance;
+                i++)
+            {
+                for (int j = pcPosition[1] - distance;
+                    j < pcPosition[1] + distance;
+                    j++)
+                {
+                    if (GetComponent<DungeonTerrain>().IsPassable(i, j))
+                    {
+                        position.Push(new int[] { i, j });
+                    }
+                }
+            }
+            return position.ToArray();
         }
 
         private bool HasFungus(int[] position)
@@ -72,6 +142,8 @@ namespace Fungus.GameSystem.Progress
             GetComponent<NourishFungus>().CountDeath += SpawnCountDown;
 
             max = GetComponent<GameData>().GetIntData(dataNode, "MaxCount");
+            distance = GetComponent<GameData>().GetIntData(dataNode, "Distance");
+
             count = max;
             notWarned = true;
         }
